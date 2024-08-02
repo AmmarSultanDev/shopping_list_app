64568a806391f7a20fa69113f09410e917392af2
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-02 16:57:32 +0100

    Refactor code to use consistent variable naming conventions nad optimize loading data.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -31,13 +31,13 @@ class _GroceryListState extends State<GroceryList> {
     );
     final response = await http.get(url);
     final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
+    final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
       final category = categories.entries
           .firstWhere(
               (catItem) => catItem.value.title == item.value['category'])
           .value;
-      _loadedItems.add(
+      loadedItems.add(
         GroceryItem(
           id: item.key,
           name: item.value['name'],
@@ -48,15 +48,23 @@ class _GroceryListState extends State<GroceryList> {
     }
 
     setState(() {
-      _groceryItems = _loadedItems;
+      _groceryItems = loadedItems;
     });
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
+    );
+
+    if (newItem == null) return;
+
+    setState(
+      () {
+        _groceryItems.add(newItem);
+      },
     );
   }
 
